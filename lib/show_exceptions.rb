9f80f49aad20fa1708b138bcaa87ec27b51d78ab
Luke Espina <luke.espina@gmail.com>
2018-01-16 12:46:57 -0500

    Add ShowExceptions

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,29 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue
+      render_exception(env)
+    end
   end
 
   private
 
   def render_exception(e)
+    # exceptions_file = File.read('templates/rescue.html.erb')
+    # res = Rack::Response.new
+    # res.status = 500
+    # res['Content-Type'] = 'text/html'
+    # res.write('RuntimeError')
+    ['500', {'Content-type' => 'text/html'}, 'RuntimeError']
   end
 
 end
