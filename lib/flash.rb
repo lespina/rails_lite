9f0290348c3d0f9cb84edc79c5cc5e7c975373ad
Luke Espina <luke.espina@gmail.com>
2018-01-16 12:25:16 -0500

    Clean up flash implementation and add flash.now

--- lib/flash.rb
+++ lib/flash.rb
@@ -4,23 +4,24 @@ require 'active_support'
 
 class Flash
   def initialize(req)
-    @@to_delete ||= [[], []]
-    @req = req
     cookie = req.cookies['_rails_lite_app_flash']
-    @flash = (cookie) ? JSON.parse(cookie) : {}
+    @now = (cookie) ? JSON.parse(cookie) : {}
+    @flash = {}
   end
 
   def store_flash(res)
-    @flash.except!(*@@to_delete.first)
-    @@to_delete.rotate!
+    res.set_cookie(
+      '_rails_lite_app_flash',
+      { path: '/', value: JSON.generate(@flash) }
+    )
+  end
 
-    res.set_cookie '_rails_lite_app_flash', { path: '/', value: JSON.generate(@flash) }
-
-    @@to_delete[1] = @flash.keys
+  def now
+    @now
   end
 
   def [](key)
-    @flash[key.to_s]
+    @now[key.to_s] || @flash[key.to_s]
   end
 
   def []=(key, value)
