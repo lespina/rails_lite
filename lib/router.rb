a00490bc9d78d4e666d52b4cab2bf85349c9e0dd
Luke Espina <luke.espina@gmail.com>
2018-01-27 13:27:02 -0500

    Alter comments to be more descriptive

--- lib/router.rb
+++ lib/router.rb
@@ -8,13 +8,13 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
+  # checks if both the pattern matches the request path and the
+  # internal http method matches request http method
   def matches?(req)
     @pattern.match?(req.path) && @http_method.to_s.downcase == req.request_method.downcase
   end
 
-  # use pattern to pull out route params (save for later?)
-  # instantiate controller and call controller action
+  # instantiates the controller and invokes the controller action
   def run(req, res)
     match_data = @pattern.match(req.path)
     route_params = (match_data) ?
@@ -34,26 +34,25 @@ class Router
     @routes = []
   end
 
-  # simply adds a new route to the list of routes
+  # adds a route to the internal list of routes
   def add_route(pattern, method, controller_class, action_name)
     @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
+  # syntactic sugar to evaluate the proc in the
+  # context of the instance
   def draw(&proc)
     self.instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called add route
+  # dynamically defines methods for each HTTP verb at runtime
+  # that, when called, add a route with the corresponding verb
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
       add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
   def match(req)
     @routes.each do |route|
       return route if route.matches?(req)
@@ -61,7 +60,7 @@ class Router
     return nil
   end
 
-  # either throw 404 or call run on a matched route
+  # call run on a matched route or throw a 404 not found error
   def run(req, res)
     matched_route = match(req)
     if matched_route
